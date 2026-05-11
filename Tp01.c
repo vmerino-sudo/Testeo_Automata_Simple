aade612d32d9c61b0d5de86af7e17c682b760a2a
Victor Merino <https://github.com/vmerino-sudo/SSL_MerinoMunoz_VictorMiguel>
2026-05-10 23:27:58 -0300

    Integro la funcion scanner, la funcion verifColumna y la funcion finDeCadena, no testee errores todavia

--- Tp01.c
+++ Tp01.c
@@ -1,27 +1,64 @@
 #include <stdio.h>
 #include <ctype.h> 
 
-typedef enum {INICIO, Q1, FINAL} Estado;
+
+#define CENTINELA ','
+typedef enum {INICIO, Q1, ERROR} Estado;
 
 Estado reconocerNumero(const char *inputString );
-char tabla[3][3] = {
+Estado tablaTransiciones[3][3] = { //tabla[estadoActual][Columna]
 //  Digito  Coma  Otro 
-     { 0,     0,    2     }, //inicio
+     { 1,     2,    2     }, //inicio
      { 1,     0,    2     }, //Q1
-     { 1,     0,    2     }, //Fin
+     { 2,     0,    2     }, //ERROR o palabra no valida
 };
+
+void finDeCadena(Estado estado, FILE *salida){
+    if (estado == ERROR) {
+        fprintf(salida, " Aceptada\n");
+    } else {
+        fprintf(salida, " Rechazada\n");
+    }
+}
+
+
+int verifColumna(int c){ //le asigna el estado que corresponda
+    if (isdigit(c)) return 0;
+    if (c==',') return 1;
+    return 2;
+}
+
+
+
+void scanner(FILE *entrada, FILE *salida){ //revisa el archivo y envia la salida a la terminal
+    int caracter;
+    Estado estadoActual = INICIO;
+    while((caracter = fgetc(entrada)) != EOF){
+        if(caracter != CENTINELA){
+            fputc(caracter, salida);
+            estadoActual = tablaTransiciones[estadoActual][(verifColumna(caracter))]; //transicion a tabla de transiciones
+    } 
+
+    else {
+        finDeCadena(estadoActual, salida);
+        estadoActual = INICIO;
+         }
+    }
+    finDeCadena(estadoActual, salida)
+}
+
+
 
 
 int main(){
+
     printf("Programa qeu reonoce si un string es un numero decimal");
+      int caracter;
+      Estado estadoActual = INICIO; //declaro los estados donde se va a mover la palabra
     FILE* texto;
-    texto = fopen("LIBROS.DAT", "rb");
-    int caracter;
-    while(caracter=(fgetc(texto))!=EOF){
-        if(texto){
-            
-        }
-    }
+    texto = fopen("prueba.txt", "rb"); 
+    
+    scanner(texto, stdout); //el scanner revisa el archivo y realiza la salida solicitada por terminal
 
     fclose(texto);
     return 0;
